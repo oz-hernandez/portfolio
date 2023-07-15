26f3b19be5121eb53924f9ddfd3d1fcef6d381aa
Osmani-Josue Hernandez <osmani.j.hernandez@gmail.com>
2023-07-14 20:47:55 -0400

    Updates

---
+++ precache-manifest.fca110806de138df1cbbe6540615adc0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c177d0f676be3c064bc3",
+    "url": "/portfolio/static/css/main.cb63f899.chunk.css"
+  },
+  {
+    "revision": "c177d0f676be3c064bc3",
+    "url": "/portfolio/static/js/main.c177d0f6.chunk.js"
+  },
+  {
+    "revision": "02feadb2adb76b10ee9b",
+    "url": "/portfolio/static/js/1.02feadb2.chunk.js"
+  },
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "acbeb92293039ac9adde64436e7a2d70",
+    "url": "/portfolio/index.html"
+  }
+];
